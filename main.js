8b36cb5c12ae6bf29dcbd96a37cf2fef30e065a8
Jared Camins-Esakov <jcamins@cpbibliography.com>
2016-03-18 21:52:04 -0400

    Disable dev tools

--- main.js
+++ main.js
@@ -17,9 +17,6 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/node_modules/opentype.js/glyph-inspector.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
